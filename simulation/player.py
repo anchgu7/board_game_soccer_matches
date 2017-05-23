62e5c9184feb24dd462d894642ae39618fd4049a
anchgu7 <anchguerra@gmail.com>
2017-05-22 22:38:38 -0700

    Template for player.cs

---
+++ simulation/player.py
@@ -0,0 +1,67 @@
+# This file defines the various types of Players that can be playing in a Game. Each Player is different based on their
+# Position, however each Player also has similarities. Therefore, each Individual Player inherits from the base Player
+# class.
+
+# Author: Andrew Guerra
+
+class Player:
+
+    def __init__(self):
+        # Each Player has the following member variables:
+        # 1. A "D12" (Dice Object) that is rolled for its action.
+        # 2. The "number of rolls" (int) the Player can roll each time they have to roll.
+        # 3. Three booleans, that are results of rolling the Reckless Die.
+        #       1. "Injured" --> the Player becomes injured
+        #       2. "Ejected" --> the Player gets a Red Card
+        #       3. "Warned" --> the Player gets a Yellow Card
+        self.D12 = None
+        self.number_of_rolls = 0
+        self.injured = False
+        self.ejected = False
+        self.warned = False
+
+    def offensive_play(self):
+        # This method is overridden in each subclass. It defines what the Player does when it's making a roll while
+        # on Offense.
+        pass
+
+    def defensive_play(self):
+        # This method is overridden in each subclass. It defines what the Player does when it's making a roll while
+        # on Defense.
+        pass
+
+
+class Keeper(Player):
+
+    def offensive_play(self):
+        pass
+
+    def defensive_play(self):
+        pass
+
+
+class Defender(Player):
+
+    def offensive_play(self):
+        pass
+
+    def defensive_play(self):
+        pass
+
+
+class Midfielder(Player):
+
+    def offensive_play(self):
+        pass
+
+    def defensive_play(self):
+        pass
+
+
+class Forward(Player):
+
+    def offensive_play(self):
+        pass
+
+    def defensive_play(self):
+        pass
---
+++ simulation/simulation.py
@@ -0,0 +1,5 @@
+# This is the driver file for the soccer board game. This file spawns games and plays them out based on different
+# initializations, which are read in from a file. The results of each game are then recorded and aggregated once all
+# games have been played in the simulation.
+
+# Author: Andrew Guerra
